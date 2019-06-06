c581190895f6ff62fc42e03c14359c5bf07ff68a
xinyu shan <xinyu.shan@utbm.fr>
2019-06-07 00:40:44 +0200

    coverall 13 whyyyyy

--- src/test/groovy/projet/gl51/store/DatabaseProductStorageTest.groovy
+++ src/test/groovy/projet/gl51/store/DatabaseProductStorageTest.groovy
@@ -26,7 +26,77 @@ class DatabaseProductStorageTest extends Specification {
 		expect:
 		products == []
 	}
+	void "update-->change props"() {
+		setup:
+		String id = client.toBlocking().retrieve(HttpRequest.POST('/store/product', new Product(name, description, price, idealTemperature)))
 
+		when:
+		Product otherProduct = new Product(name, description, price, idealTemperature)
+		HttpStatus status = client.toBlocking().retrieve(HttpRequest.PATCH('/store/product/' + id, otherProduct), Argument.of(HttpStatus).type)
+		Product updatedProduct = client.toBlocking().retrieve(HttpRequest.GET('/store/product/' + id), Argument.of(Product).type)
+
+		then:
+		status == OK
+		updatedProduct.getPrice() == otherProduct.getPrice()
+		updatedProduct.getName() == otherProduct.getName()
+
+		where:
+		name | description | price | idealTemperature | name1 | description1 | price1 | idealTemperature1
+		"Chips" | "delicious" | 2 | 10 | "chips lays" | "more delicious" | 20 | 10
+	}
+	void "create"(){
+		when:
+		String id = client.toBlocking().retrieve(HttpRequest.POST('/store/product', new Product(name, description, price, idealTemperature)))
+		Product productReturned = client.toBlocking().retrieve(HttpRequest.GET('/store/product/' + id), Argument.of(Product).type)
+
+		then:
+		id != ""
+		productReturned.getId() == id
+
+		where:
+		name | description | price | idealTemperature
+		"Parapluie" | "Très utile" | 12 | 5
+		/*when:
+		String id = client.toBlocking().retrieve(HttpRequest.POST('/store/product', new Product(name, description, price, idealTemperature)))
+		Product productReturned = client.toBlocking().retrieve(HttpRequest.GET('/store/product/' + id), Argument.of(Product).type)
+
+		then:
+		id != ""
+		productReturned.getId() == id
+		
+		where:
+		name | description | price | idealTemperature
+		"Chips" | "delicious" | 2 | 10*/
+	}
+	
+	void "delete"(){
+		setup:
+		String id = client.toBlocking().retrieve(HttpRequest.POST('/store/product', new Product(name, description, price, idealTemperature)))
+
+		when:
+		HttpStatus status = client.toBlocking().retrieve(HttpRequest.DELETE('/store/product/' + id), Argument.of(HttpStatus).type)
+		Product productReturned = client.toBlocking().retrieve(HttpRequest.GET('/store/product/' + id), Argument.of(Product).type)
+
+		then:
+		status == OK
+		productReturned == null
+		thrown HttpClientResponseException
+
+		where:
+		name | description | price | idealTemperature
+		"Chips" | "delicious" | 2 | 10
+	}
+
+	void "deleting non-exist"(){
+		setup:
+		String id = UUID.randomUUID().toString()
+
+		when:
+		HttpStatus status = client.toBlocking().retrieve(HttpRequest.DELETE('/store/product/' + id), Argument.of(HttpStatus).type)
+
+		then:
+		thrown HttpClientResponseException
+	}
 	
 
 	
